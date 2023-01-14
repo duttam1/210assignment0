5747ea756255ef0286b1a13c64cf8e5b11e540d8
Milan Dutta <111601092+duttam1@users.noreply.github.com>
2023-01-14 15:58:28 -0500

    Add files via upload

---
+++ assignment-0-duttam1 copy/src/a0/Main.java
@@ -0,0 +1,20 @@
+package a0;
+
+//Comment to fix my GitHub
+public class Main {
+    public static void main(String[] args) {
+        // Prints out your Java version
+        // Make sure it is 10.0 or higher
+        System.out.println("Your Java version: " + System.getProperty(("java.version")));
+
+        // Task 2 here
+        sayHello();
+    }
+
+    // Task 1 here
+    public static void sayHello() {
+        System.out.println("Hello World");
+    }
+
+
+}
